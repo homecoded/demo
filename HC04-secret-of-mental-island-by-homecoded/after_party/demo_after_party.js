5149ce7a30d84c9564a0a7bcf5dac05cca8e6016
Mathieu 'p01' Henri <mathieu@p01.org>
2016-03-30 09:54:57 +0200

    900 bytes version with smoother gradient
    Shuffled the display of the clouds to save some font settings used for the text
    Draw the gradient using the bgColor of the page and subpixel horizontal lines
    Combined drawing of the water front and Melee island

--- HC04-secret-of-mental-island-by-homecoded/after_party/demo_after_party.js
+++ HC04-secret-of-mental-island-by-homecoded/after_party/demo_after_party.js
@@ -1,50 +1,25 @@
-//function asd () {
-//    var
-        A = new AudioContext,
-        C = A.createScriptProcessor(a.width = 1024, 1, 1),
-        s = 0, t = 0, C.connect(A.destination),
-        C.onaudioprocess = function (l) {
-            for (b = l.outputBuffer.getChannelData(x=0),
-                     a.height = 370,
-                     c.fillStyle = "#214",
-                     c.fillRect(0, x, 1024, 370),
-                     c.fillStyle = "#46f",
-                     c.globalAlpha = .1,
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.fillRect(0, x, 1024, x += 37),
-                     c.globalAlpha = 1,
-                     c.font = "62px serif",
-                     c.fillText("☁", (4*s%1400) - 300, 220),
-                     c.fillText("☁", (6*s%1400) - 300, 220),
-                     c.font = "110px serif",
-                     c.fillText("☁", (7*s%1400) - 300, 220),
-                     c.font = "220px serif",
-                     c.fillText("☁", (9*s%1400)- 300, 220),
-                     c.fillStyle = "#002", x=0;
-                 x < 1024;
-                 x++,
-                     s += 6.25 / A.sampleRate,
-                     t ++)
-                c.fillRect(x, 330 + Math.min(277 * Math.sin(x / 117) + 4 * Math.sin(x / 11)), 1, 370),
-                    k = 55 * Math.pow(1.059463, parseInt("bbnqbbnn99lljjvzeeqqeellggggsnnzbbnnbbnn99lljjvzeeqqeeqqggggllxxlllleeqqaamybbnn99lleeii66iibbnq99lo77jn77jj77jneeqqggssnnnnbbnqeennllllbbnueenn99lpjjjjjjjjjjjjjjeeiillllll77lu11449999bbbb6622222222bbccggjjjjjjjjjjoonniieeeeeeeeeeebeeiillllllllllqqppllggggggggggll"[0 | s % 264], 36)),
-                    l = 55 * Math.pow(1.059463, parseInt("    qqqtsqooqqqqqqooomlommmmllllllqqqqqtsqooqqqqqqqsttttvvvvssstsqosttttssssqqqtsqosttttssssqqqtsqooqqqqqqqqqqqqomlommmmllllllllllllllllllllllllllooqqhhhhhhmljmoohhhhhhllhhmmooooovovtvttssqqllllllgghhrrrrrrrrrrrrrrrrxxttttttssttstqtttmmmmmmmmmmmmmmjjggggggeeggegcc"[0 | s % 264], 36)+9),
-                    b[x] = (Math.sin(Math.PI * t / A.sampleRate * l * 2) >0 ?.5: -.5)
-                        + (Math.sin(Math.PI * t / A.sampleRate * k * 2)   >0 ?.5: -.5),
-                c.fillRect(x, 330 + b[x], 1, 370)
-            ;
-
-            c.fillStyle = "#B4B",
-                c.font = "16px serif",
-                c.fillText("THE SECRET OF", 480, 98),
-                c.font = "62px serif",
-                c.fillText("MENTAL", 420, 150),
-                c.fillText("ISLAND", 480, 196)
-        }
-//}();
+// Compresses to 900 bytes ;)
+b.bgColor = "#36f",
+A = new AudioContext,
+C = A.createScriptProcessor(a.width = 1024, s = 0, 1),
+C.connect(A.destination),
+C.onaudioprocess = function(b) {
+  for (a.height = 480,
+  b = b.outputBuffer.getChannelData(d = 0),
+  c.fillStyle = "#9cf",
+  c.font = "116px serif",
+  c.fillText("☁", 7 * s % 1336 - 226, 226),
+  c.font = "226px serif",
+  c.fillText("☁", 9 * s % 1336 - 226, 226),
+  c.font = "66px serif",
+  c.fillText("☁", 4 * s % 1336 - 226, 226),
+  c.fillText("☁", 6 * s % 1336 - 226, 226),
+  c.fillStyle = "#003"; d < 1024; s += 6.25 / A.sampleRate)
+    c.fillRect(0, d, 1024, d < 336 ? 1 - d / 336 : 1),
+    c.fillRect(d, 336 + (b[d++] = (55 * s * Math.pow(1.059463, parseInt("    qqqtsqooqqqqqqooomlommmmllllllqqqqqtsqooqqqqqqqsttttvvvvssstsqosttttssssqqqtsqosttttssssqqqtsqooqqqqqqqqqqqqomlommmmllllllllllllllllllllllllllooqqhhhhhhmljmoohhhhhhllhhmmooooovovtvttssqqllllllgghhrrrrrrrrrrrrrrrrxxttttttssttstqtttmmmmmmmmmmmmmmjjggggggeeggegccbbnqbbnn99lljjvzeeqqeellggggsnnzbbnnbbnn99lljjvzeeqqeeqqggggllxxlllleeqqaamybbnn99lleeii66iibbnq99lo77jn77jj77jneeqqggssnnnnbbnqeennllllbbnueenn99lpjjjjjjjjjjjjjjeeiillllll77lu11449999bbbb6622222222bbccggjjjjjjjjjjoonniieeeeeeeeeeebeeiillllllllllqqppllggggggggggll"[0 | s % 264 + (d & 1) * 264], 36) + 9) & 1) - .5), 4, 226 * Math.sin(d / 116) + 16 * Math.sin(d / 16));
+  c.fillStyle = "#f6f",
+  c.fillText("MENTAL", 420, 146),
+  c.fillText("ISLAND", 480, 196),
+  c.font = "16px serif",
+  c.fillText("THE SECRET OF", 480, 96)
+}
