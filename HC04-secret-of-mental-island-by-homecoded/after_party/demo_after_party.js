d872f4b3db047fe6f3e71e323e9b6639a02e1ec7
Manuel Rülke <manuel.ruelke@sitewards.com>
2016-03-30 22:59:35 +0200

    fixed pitch problem
    - made song a little slower, that saved a bit
    - lowered the base frequency for better sound
    - fixed the 9-halftone-off problem with the second track

--- HC04-secret-of-mental-island-by-homecoded/after_party/demo_after_party.js
+++ HC04-secret-of-mental-island-by-homecoded/after_party/demo_after_party.js
@@ -1,4 +1,4 @@
-// Compresses to 900 bytes ;)
+// compress using regpack
 b.bgColor = "#36f",
 A = new AudioContext,
 C = A.createScriptProcessor(a.width = 1024, s = 0, 1),
@@ -14,9 +14,9 @@ C.onaudioprocess = function(b) {
   c.font = "66px serif",
   c.fillText("☁", 4 * s % 1336 - 226, 226),
   c.fillText("☁", 6 * s % 1336 - 226, 226),
-  c.fillStyle = "#003"; d < 1024; s += 6.25 / A.sampleRate)
+  c.fillStyle = "#003"; d < 1024; s += 6 / A.sampleRate)
     c.fillRect(0, d, 1024, d < 336 ? 1 - d / 336 : 1),
-    c.fillRect(d, 336 + (b[d++] = (55 * s * Math.pow(1.059463, parseInt("    qqqtsqooqqqqqqooomlommmmllllllqqqqqtsqooqqqqqqqsttttvvvvssstsqosttttssssqqqtsqosttttssssqqqtsqooqqqqqqqqqqqqomlommmmllllllllllllllllllllllllllooqqhhhhhhmljmoohhhhhhllhhmmooooovovtvttssqqllllllgghhrrrrrrrrrrrrrrrrxxttttttssttstqtttmmmmmmmmmmmmmmjjggggggeeggegccbbnqbbnn99lljjvzeeqqeellggggsnnzbbnnbbnn99lljjvzeeqqeeqqggggllxxlllleeqqaamybbnn99lleeii66iibbnq99lo77jn77jj77jneeqqggssnnnnbbnqeennllllbbnueenn99lpjjjjjjjjjjjjjjeeiillllll77lu11449999bbbb6622222222bbccggjjjjjjjjjjoonniieeeeeeeeeeebeeiillllllllllqqppllggggggggggll"[0 | s % 264 + (d & 1) * 264], 36) + 9) & 1) - .5), 4, 226 * Math.sin(d / 116) + 16 * Math.sin(d / 16));
+    c.fillRect(d, 336 + (b[d++] = (22 * s * Math.pow(1.059463, parseInt("    qqqtsqooqqqqqqooomlommmmllllllqqqqqtsqooqqqqqqqsttttvvvvssstsqosttttssssqqqtsqosttttssssqqqtsqooqqqqqqqqqqqqomlommmmllllllllllllllllllllllllllooqqhhhhhhmljmoohhhhhhllhhmmooooovovtvttssqqllllllgghhrrrrrrrrrrrrrrrrxxttttttssttstqtttmmmmmmmmmmmmmmjjggggggeeggegccbbnqbbnn99lljjvzeeqqeellggggsnnzbbnnbbnn99lljjvzeeqqeeqqggggllxxlllleeqqaamybbnn99lleeii66iibbnq99lo77jn77jj77jneeqqggssnnnnbbnqeennllllbbnueenn99lpjjjjjjjjjjjjjjeeiillllll77lu11449999bbbb6622222222bbccggjjjjjjjjjjoonniieeeeeeeeeeebeeiillllllllllqqppllggggggggggll"[0 | s % 264 + (d & 1) * 264], 36) + 9 * (1 - d & 1) ) & 1) -.5), 4, 226 * Math.sin(d / 116) + 16 * Math.sin(d / 16));
   c.fillStyle = "#f6f",
   c.fillText("MENTAL", 420, 146),
   c.fillText("ISLAND", 480, 196),
